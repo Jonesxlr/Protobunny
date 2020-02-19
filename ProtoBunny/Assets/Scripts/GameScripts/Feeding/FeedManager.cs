f2391b23c0cd987ab12ecc51e8388f53ec92009a
Jonesxlr <39425389+Jonesxlr@users.noreply.github.com>
2020-02-19 17:40:27 -0500

    Started working on Feeding

---
+++ ProtoBunny/Assets/Scripts/GameScripts/Feeding/FeedManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedManager : MonoBehaviour
+{
+    private float study=1.5f, wait=4f;
+    private GameInfos infos;
+    public GameObject OptOne, OptTwo, OptThr, OptFor, OptFiv;
+    private int chosen,picked,progress,max;
+    private bool ready = false,waiting=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        infos = GetComponent<GameInfos>();
+        max = 4 + infos.GetDay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            ready = true;
+
+        if (ready)
+        {
+            if(!waiting)
+                study -= Time.deltaTime;
+            if (study <= 0)
+            {
+                waiting = true;
+                study = 1.5f;
+            }
+            if (waiting)
+            {
+                wait -= Time.deltaTime;
+                if (wait <= 0)
+                {
+                    Wrong();
+                }
+            }
+        }
+    }
+
+    public void Choice(GameObject sel)
+    {
+        waiting = false;
+        wait = 4.0f;
+        if (sel == OptOne)
+        {
+            chosen = 1;
+        }
+        else
+        {
+            if (sel == OptTwo)
+            {
+                chosen = 2;
+            }
+            else
+            {
+                if(sel == OptThr)
+                {
+                    chosen = 3;
+                }
+                else
+                {
+                    if (sel == OptFor)
+                    {
+                        chosen = 4;
+                    }
+                    else
+                    {
+                        if (sel == OptFiv)
+                        {
+                            chosen = 5;
+                        }
+                    }
+                }
+            }
+        }
+        CompareChoice();
+    }
+
+    private void pickFood()
+    {
+        float chose = Random.Range(1f, 5f);
+        picked = Mathf.RoundToInt(chose);
+        Debug.Log(picked);
+        SetDisplay();
+    }
+
+    private void CompareChoice()
+    {
+        if (chosen == picked)
+        {
+            Correct();
+        }
+        else
+            Wrong();
+    }
+
+    private void Correct()
+    {
+        progress++;
+        if (progress >= max)
+        {
+            Debug.Log("Feeding Done");
+            infos.NextActivity();
+        }
+    }
+
+    private void Wrong()
+    {
+        progress--;
+    }
+
+    private void SetDisplay()
+    {
+        //wait
+    }
+}
---
+++ ProtoBunny/Assets/Scripts/GameScripts/Feeding/Opt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opt : MonoBehaviour
+{
+    public FeedManager Feedie;
+
+    private void OnMouseDown()
+    {
+        Feedie.Choice(gameObject);
+    }
+}
